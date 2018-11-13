232ffa081158c2587684acba074cc45f6f8af3bd
Keisuke Shimotori <destrage1958@gmail.com>
2018-11-13 23:08:14 +0900

    Vue instance

--- index.js
+++ index.js
@@ -68,3 +68,16 @@ var app7 = new Vue({
     ]
   }
 })
+
+var data = { a : 1 };
+var vm = new Vue({
+  el: "#example",
+  data: data
+})
+
+vm.$data === data;
+vm.$el === document.getElementById("example");
+
+vm.$watch("a", function(newValue, oldValue) {
+  
+})
